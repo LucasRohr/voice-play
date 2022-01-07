110f759a44e3dfe464838f44742476628ca2470f
Lucas Rohr Carreno <lucasrc17@live.com>
2022-01-07 18:08:43 -0300

    feat: :sparkles: adding project base

---
+++ App.tsx
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+  Text,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+          <Header />
+          <Text>Voice Play</Text>
+        </ScrollView>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- README.md
+++
@@ -1,2 +0,0 @@
-# voice-play
-A React Native application that can be totally navigated and operated using voice commands.
